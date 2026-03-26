afd51a0f996b6ef2e3cd5e64a6bcce78cfd947c6
Mayank-Jangid02 <mayankjangid0105@gmail.com>
2026-03-26 21:26:39 +0530

    Delete backend directory

--- backend/routes/blog.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getblog, createblog ,deleteblog} from "../controllers/blog.controller.js";    
-let router = Router();
-
-// Define your blog routes here
-router.get("/getblog",getblog);
-router.post("/createblog",createblog);
-router.delete("/deleteblog/:id",deleteblog);
-export default router;
