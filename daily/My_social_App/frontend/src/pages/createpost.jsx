59faabdc5f6e328e7d0b83401bd15ac462f5ae90
Mayank-Jangid02 <mayankjangid0105@gmail.com>
2026-04-02 11:17:26 +0530

    Delete daily/My_social_App directory

--- daily/My_social_App/frontend/src/pages/createpost.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-export default function createpost() {
-  const navigate = useNavigate();
-  return (
-    <div>createpost</div>
-  )
-}
--- daily/My_social_App/frontend/src/pages/feed.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-export default function feed() {
-  const navigate = useNavigate();
-  return (
-    <div>feed</div>
-  )
-}
