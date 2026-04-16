808ab7c2074daeb3f4b792f371fa5d2dc05fd355
Mayank Jangid <mayankjangid0105@gmail.com>
2026-04-16 22:33:47 +0530

    update post controller and post route for the like and comment functionality

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -57,3 +57,98 @@ export async function deletePost(req, res) {
         res.status(500).json({ message: error.message });
     }
 }
+
+export async function likepost(req,res){
+    try {
+        const {id} = req.params;
+        const post = await Post.findById(id);
+        if (post) {
+            post.likes.push(req.body); 
+            await post.save();
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: "Post not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export async function commentpost(req,res) {
+    
+    try{
+        const {id} = req.params;
+        const post = await Post.findById(id);
+        if (post) {
+            post.comments.push(req.body);
+            await post.save();
+            res.status(200).json(post);
+        }else {
+            res.status(404).json({ message: "Post not found" });
+        }
+    }catch(error){
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export async function deletecomment(req,res)
+{
+    try{
+       const {id}=req.params;
+       const post=  await Post.findById(id);
+       if(post){
+        post.comments.pull(req.body);
+        await post.save();
+        res.status(200).json(post);
+       }else{
+        res.status(404).json({ message: "Post not found" });
+       }
+    }catch(error){
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export async function deleteLike(req,res)
+{
+    try{
+       const {id}=req.params;
+       const post=  await Post.findById(id);
+       if(post){
+        post.likes.pull(req.body);
+        await post.save();
+        res.status(200).json(post);
+       }else{
+        res.status(404).json({ message: "Post not found" });
+       }
+    }catch(error){
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export async function getallLike(req,res) {
+    try{
+     const {id}=req.params;
+     const post=await Post.findById(id);
+     if(post){
+        res.status(200).json(post.likes);
+     }else{
+        res.status(404).json({message:"Post not found"});
+     }
+    }catch(error){
+        res.status(500).json({message:error});
+    }
+}
+
+export async function getallComment(req,res) {
+    try{
+     const {id}=req.params;
+     const post=await Post.findById(id);
+     if(post){
+        res.status(200).json(post.comments);
+     }else{
+        res.status(404).json({message:"Post not found"});
+     }
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+}
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllPost, createPost, getPostById, updatePost, deletePost } from "../controllers/post.controller.js";
+import { getAllPost, createPost, getPostById, updatePost, deletePost,likepost, commentpost, deletecomment, deleteLike, getallLike, getallComment  } from "../controllers/post.controller.js";
 const router = Router();
 
 router.get('/getallpost',getAllPost);
@@ -7,4 +7,11 @@ router.post('/createpost',createPost);
 router.get('/getpost/:id',getPostById);
 router.put('/updatepost/:id',updatePost);
 router.delete('/deletepost/:id',deletePost);
+router.post('/like/:id',likepost);
+router.post('/comment/:id',commentpost);
+router.delete('/comment/:id',deletecomment);
+router.delete('/like/:id',deleteLike);
+router.get('/like/:id',getallLike);
+router.get('/comment/:id',getallComment);
+
 export default router;
